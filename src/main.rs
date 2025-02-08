183648223624380faacac9b5d9057350b2c4f98f
Robin Truvelot <truvelotr@gmail.com>
2025-02-08 18:36:48 +0100

    begin fct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs;
+use std::path::Path;
 fn main() {
-    println!("Hello, world!");
+    let dossier = "test";
+    if !Path::new(dossier).exists()
+    {
+        fs::create_dir(dossier).expect("error creation dossier");
+        println!("dossier créé");
+    }
+    else
+    {
+        println!("dossier existe");
+    }
+
 }
+
