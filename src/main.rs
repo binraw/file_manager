927d4e37fe77b78f004c94fd933b437c0092311d
Robin Truvelot <truvelotr@gmail.com>
2025-02-10 19:57:12 +0100

    begin algo_range

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
-use std::fs;
-use std::path::{Path, PathBuf};
+
+use utils::lib::algo_range;
 mod utils;
 
-fn main() {
-
-
-}
-
-fn move_file(file: &str, dossier: &str)
+fn main() 
 {
-    if !Path::new(dossier).exists()
-    {
-        fs::create_dir(dossier).expect("error creation dossier");
-        println!("dossier créé");
-        let file_destination = PathBuf::from(dossier).join(file);
-        fs::rename(file, &file_destination).expect("Impossible de déplacer le fichier");
-    }
+    algo_range();
 }
+
--- src/utils/lib.rs
+++ src/utils/lib.rs
@@ -1,12 +1,13 @@
-use std::fs::{self, metadata};
+
+use std::fs::{self};
 use std::path::{Path, PathBuf};
 
 #[derive(Debug, PartialEq)]
 pub enum Extension {
-    png,
-    pdf,
-    txt,
-    img,
+    Png,
+    Pdf,
+    Txt,
+    Img,
 
 }
 
@@ -22,8 +23,29 @@ pub fn move_file(file: &str, dossier: &str)
 }
 
 pub fn algo_range()
-{
+{ // ca ne fonctionne pas ca boucle sur les extension et quand ca trouve la bonne ca stop le programme
+    // donc fonctionne sur un seul file pour l'instand
+    let path = "./";
 
+    if let Ok(entries) = fs::read_dir(path) 
+    {
+        let mut file_count = 0;
+        for entry in entries 
+        {
+            if let Ok(entry) = entry 
+            {
+                let catalog = entry.path();  
+                let file = catalog.display().to_string();
+                file_count += 1;
+                println!("{}", file_count); 
+                for ext in Extension::all() 
+                {
+                    print!("{}",file);
+                    control_typefile(&file, ext);
+                }
+            }
+        }
+    }
 }
 
 pub fn control_typefile(file: &str, ext:Extension)
@@ -36,11 +58,11 @@ pub fn control_typefile(file: &str, ext:Extension)
     }
     else if file_type.is_dir()
     {
-
+        return;
     }
     else 
     {
-        
+        return;
     }
 }
 
@@ -48,12 +70,23 @@ impl Extension {
     
     fn from_str(ext: &str) -> Option<Extension> {
         match ext.to_lowercase().as_str() {
-            "png" => Some(Extension::png),
-            "pdf" => Some(Extension::pdf),
-            "txt" => Some(Extension::txt),
-            "img" => Some(Extension::img),
+            "png" => Some(Extension::Png),
+            "pdf" => Some(Extension::Pdf),
+            "txt" => Some(Extension::Txt),
+            "img" => Some(Extension::Img),
             _ => None,
         }
+    }
+}
+
+impl Extension {
+    pub fn all() -> Vec<Extension> {
+        vec![
+            Extension::Png,
+            Extension::Pdf,
+            Extension::Txt,
+            Extension::Img,
+        ]
     }
 }
 
@@ -65,10 +98,10 @@ pub fn control_extension(file: &str, ext: Extension)
         Some(extension_str) => match Extension::from_str(extension_str) {
             Some(file_extension) if file_extension == ext => {
                 match file_extension {
-                    Extension::png => move_file(file, "PNG_Collection"),
-                    Extension::pdf => move_file(file, "PDF_Collection"),
-                    Extension::txt => move_file(file, "TXT_Collection"),
-                    Extension::img => move_file(file, "IMG_Collection"),
+                    Extension::Png => move_file(file, "PNG_Collection"),
+                    Extension::Pdf => move_file(file, "PDF_Collection"),
+                    Extension::Txt => move_file(file, "TXT_Collection"),
+                    Extension::Img => move_file(file, "IMG_Collection"),
                 }
             }
             Some(_) => println!("L'extension ne correspond pas à ce que l'on attend ❌"),
