019dab5c9eee06fde19329c484e6791cd5568591
Robin Truvelot <truvelotr@gmail.com>
2025-02-11 19:54:12 +0100

    fix algo

--- src/utils/lib.rs
+++ src/utils/lib.rs
@@ -3,7 +3,8 @@ use std::fs::{self};
 use std::path::{Path, PathBuf};
 
 #[derive(Debug, PartialEq)]
-pub enum Extension {
+pub enum Extension 
+{
     Png,
     Pdf,
     Txt,
@@ -22,31 +23,37 @@ pub fn move_file(file: &str, dossier: &str)
     fs::rename(file, &file_destination).expect("Impossible");
 }
 
-pub fn algo_range()
-{ // ca ne fonctionne pas ca boucle sur les extension et quand ca trouve la bonne ca stop le programme
-    // donc fonctionne sur un seul file pour l'instand
+pub fn algo_range() 
+{
     let path = "./";
-
-    if let Ok(entries) = fs::read_dir(path) 
+    if let Ok(entries) = fs::read_dir(path)
     {
-        let mut file_count = 0;
-        for entry in entries 
+        for entry in entries
         {
             if let Ok(entry) = entry 
             {
                 let catalog = entry.path();  
-                let file = catalog.display().to_string();
-                file_count += 1;
-                println!("{}", file_count); 
-                for ext in Extension::all() 
+                let file = catalog.display().to_string(); 
+                if let Some(extension_str) = catalog.extension().and_then(|e| e.to_str())
                 {
-                    print!("{}",file);
-                    control_typefile(&file, ext);
+                    if let Some(ext) = Extension::from_str(extension_str)
+                    {
+                        control_typefile(&file, ext);
+                    } 
+                    else 
+                    {
+                        println!("❌ Extension inconnue pour : {}", file);
+                    }
+                } 
+                else
+                {
+                    println!("❌ Le fichier {} n'a pas d'extension", file);
                 }
             }
         }
     }
 }
+
 
 pub fn control_typefile(file: &str, ext:Extension)
 {
@@ -66,10 +73,13 @@ pub fn control_typefile(file: &str, ext:Extension)
     }
 }
 
-impl Extension {
+impl Extension 
+{
     
-    fn from_str(ext: &str) -> Option<Extension> {
-        match ext.to_lowercase().as_str() {
+    fn from_str(ext: &str) -> Option<Extension> 
+    {
+        match ext.to_lowercase().as_str() 
+        {
             "png" => Some(Extension::Png),
             "pdf" => Some(Extension::Pdf),
             "txt" => Some(Extension::Txt),
@@ -79,8 +89,10 @@ impl Extension {
     }
 }
 
-impl Extension {
-    pub fn all() -> Vec<Extension> {
+impl Extension 
+{
+    pub fn all() -> Vec<Extension> 
+    {
         vec![
             Extension::Png,
             Extension::Pdf,
@@ -94,10 +106,14 @@ pub fn control_extension(file: &str, ext: Extension)
 {
     let path = Path::new(file);
 
-    match path.extension().and_then(|file_ext| file_ext.to_str()) {
-        Some(extension_str) => match Extension::from_str(extension_str) {
-            Some(file_extension) if file_extension == ext => {
-                match file_extension {
+    match path.extension().and_then(|file_ext| file_ext.to_str()) 
+    {
+        Some(extension_str) => match Extension::from_str(extension_str) 
+        {
+            Some(file_extension) if file_extension == ext => 
+            {
+                match file_extension 
+                {
                     Extension::Png => move_file(file, "PNG_Collection"),
                     Extension::Pdf => move_file(file, "PDF_Collection"),
                     Extension::Txt => move_file(file, "TXT_Collection"),
